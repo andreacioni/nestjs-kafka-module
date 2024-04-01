ca53f0fc2e5dc63bedc62a52a9c7f52604cd688e
Andrea Cioni <supercionci@hotmail.it>
2024-04-01 22:06:01 +0200

    add canary v8

---
+++ canary/nest-v8/test/app.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { KAFKA_ADMIN_CLIENT_PROVIDER, KafkaModule } from "nestjs-kafka-module";
+import { IAdminClient, KafkaConsumer, Producer } from "node-rdkafka";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
+
+describe("AppController (e2e)", () => {
+  let app: INestApplication;
+  let consumer: KafkaConsumer;
+  let producer: Producer;
+  let adminClient: IAdminClient;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        KafkaModule.forRoot({
+          consumer: {
+            conf: {
+              "group.id": "nestjs-rdkafka-test",
+              "metadata.broker.list": "127.0.0.1:9092",
+            },
+          },
+          producer: {
+            conf: {
+              "metadata.broker.list": "127.0.0.1:9092",
+            },
+          },
+          adminClient: {
+            conf: {
+              "metadata.broker.list": "127.0.0.1:9092",
+            },
+          },
+        }),
+        AppModule,
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    producer = app.get<Producer>(Producer);
+    consumer = app.get<KafkaConsumer>(KafkaConsumer);
+    adminClient = app.get<IAdminClient>(KAFKA_ADMIN_CLIENT_PROVIDER);
+  });
+
+  it("/ (GET)", () => {
+    return request(app.getHttpServer())
+      .get("/")
+      .expect(200)
+      .expect("Hello World!");
+  });
+
+  describe("root", () => {
+    it('should be defined"', () => {
+      expect(consumer).toBeDefined();
+      expect(producer).toBeDefined();
+      expect(adminClient).toBeDefined();
+    });
+  });
+});
